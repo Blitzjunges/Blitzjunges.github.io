45f7b5ab30fbc3d8db0a140893bdd8d84c5a5a36
Blitzjunges <107757598+Blitzjunges@users.noreply.github.com>
2024-07-14 19:05:35 +0200

    fixed bug in the hyperlink in the index.html of the homepage

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ var inhaltTabelle = {
 document.getElementById("tabelle").innerHTML = "";
 for(let i = 0; i < inhaltTabelle.name.length; i++){
   if (i%2 == 0){
-    document.getElementById("tabelle").innerHTML += '<tr class="table-light"><td><a href="blitzjunges.github.io/'+inhaltTabelle.link[i]+'/index.html" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">'+inhaltTabelle.name[i]+'</a></td></tr>';
+    document.getElementById("tabelle").innerHTML += '<tr class="table-light"><td><a href="blitzjunges.github.io/'+inhaltTabelle.link[i]+'" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">'+inhaltTabelle.name[i]+'</a></td></tr>';
   }
   if (i%2 == 1){
-    document.getElementById("tabelle").innerHTML += '<tr class="table-primary"><a href="blitzjunges.github.io/'+inhaltTabelle.link[i]+'/index.html" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">'+inhaltTabelle.name[i]+'</a></tr>';
+    document.getElementById("tabelle").innerHTML += '<tr class="table-primary"><a href="blitzjunges.github.io/'+inhaltTabelle.link[i]+'" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">'+inhaltTabelle.name[i]+'</a></tr>';
   }
 }
